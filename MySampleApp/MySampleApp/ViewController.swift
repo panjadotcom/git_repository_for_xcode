66dae2dc4ee7c6bba7df2f1bfca23a86dd742f9f
Pankaj Yadav <panjadotcom@icloud.com>
2019-04-05 13:53:00 +0530

    My First swift commit

---
+++ MySampleApp/MySampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySampleApp
+//
+//  Created by Pankaj Yadav on 05/04/19.
+//  Copyright © 2019 Cdot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
